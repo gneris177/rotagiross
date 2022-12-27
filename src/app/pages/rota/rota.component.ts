85bb6adb411663295c619636ecc874d76157cead
gnerisgiross <giross.dev.04@gmail.com>
2022-12-27 13:29:45 -0300

    feat: criação da página de rota

---
+++ src/app/pages/rota/rota.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rota',
+  templateUrl: './rota.component.html',
+  styleUrls: ['./rota.component.css']
+})
+export class RotaComponent {
+
+}
