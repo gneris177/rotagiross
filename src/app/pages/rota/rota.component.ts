65e0f7347a439aa44437dc91a5796a81c23e28e1
gnerisgiross <giross.dev.04@gmail.com>
2022-12-28 17:53:19 -0300

    feat: criando página de rota

--- src/app/pages/rota/rota.component.ts
+++ src/app/pages/rota/rota.component.ts
@@ -1,10 +1,13 @@
+import { LocationService } from './../../services/location.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rota',
   templateUrl: './rota.component.html',
-  styleUrls: ['./rota.component.css']
+  styleUrls: ['./rota.component.css'],
 })
 export class RotaComponent {
+  constructor(private locationService: LocationService) {}
 
+  ngOnInit() {}
 }
