1815cac49421057c7976418c544c1458a0fb39a4
gnerisgiross <giross.dev.04@gmail.com>
2022-12-28 17:47:07 -0300

    Fix: Corrigindo rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { RotaComponent } from './pages/rota/rota.component';
 
 const routes: Routes = [
   { path: 'rota', component: RotaComponent },
+  { path: '', component: AppComponent },
 ];
 
 @NgModule({
