6dcf4f37c29fb118cb85decc90ea28ae4025cd78
gnerisgiross <giross.dev.04@gmail.com>
2022-12-28 17:54:51 -0300

    feat: add importações e configurações necessárias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { GoogleMapsModule } from '@angular/google-maps'
 import { AppComponent } from './app.component';
+import { RotaComponent } from './pages/rota/rota.component';
+import { NavbarleftComponent } from './components/navbarleft/navbarleft.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ViewMapComponent } from './components/view-map/view-map.component';
+import { LocationService } from './services/location.service';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RotaComponent,
+    NavbarleftComponent,
+    HeaderComponent,
+    ViewMapComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    GoogleMapsModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
